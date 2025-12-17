6cb0ef2492ab2d13ef468e8c6973b3bf645d9240
Fedalitos2 <kolyan2364@gmail.com>
2025-12-17 13:43:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2386,4 +2386,6 @@ def callback():
     return 'ok'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
